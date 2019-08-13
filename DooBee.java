d7e703a70f4125e29552c7132e5a0c5ce5f89ec5
johnbel458 <47404011+johnbel458@users.noreply.github.com>
2019-08-13 10:06:25 -0400

    Add files via upload

---
+++ DooBee.java
@@ -0,0 +1,24 @@
+public class DooBee {
+
+    public static void main(String[] args) {
+        // write your code here
+        int x=1;
+        while (x<3)
+        {
+            System.out.print("I do nothing ");
+            System.out.print("and I do something now, ");
+            x=x+1;
+
+        }
+        if (x==3)
+        {
+            System.out.print("Oh I don't repeat the same thing as before the two sentence ");
+        }
+
+
+
+        }
+
+    }
+
+

be3df414665f593bd2ebc2bdf46a9b9cf01fe7a6
johnbel458 <47404011+johnbel458@users.noreply.github.com>
2019-08-15 00:00:23 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Yo check this out a");
+        System.out.println("I rule the world in case you didn't know");
+        System.out.println("In the next decade, I will have control over everything on the internet");
+        System.out.println("Mwhahhahahhah");
+        //x is assign with value of 69
+        int x =69;
+        //assign is assign with value potato
+        char assign= 'x';
+        int b=Character.getNumericValue(assign);
+        b=b+36;
+        System.out.println("lmaoo yo looks this just converted from" +" " +assign +" to " + b);
+        System.out.println("Isn't that insane");
+        int z =0;
+        while ( z<69 )
+        { z++;
+            System.out.print("Now I'm repeating" +" "+ b +":");
+
+            System.out.println(z);
+
+        }
+
+        
+
+
+
+    }
+}

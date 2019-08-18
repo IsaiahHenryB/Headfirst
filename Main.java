7e3ca72cb9531d718f17d74b82cc2244d5acd6b6
johnbel458 <47404011+johnbel458@users.noreply.github.com>
2019-08-18 15:59:04 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,29 +3,34 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Yo check this out a");
-        System.out.println("I rule the world in case you didn't know");
-        System.out.println("In the next decade, I will have control over everything on the internet");
-        System.out.println("Mwhahhahahhah");
-        //x is assign with value of 69
-        int x =69;
-        //assign is assign with value potato
-        char assign= 'x';
-        int b=Character.getNumericValue(assign);
-        b=b+36;
-        System.out.println("lmaoo yo looks this just converted from" +" " +assign +" to " + b);
-        System.out.println("Isn't that insane");
-        int z =0;
-        while ( z<69 )
-        { z++;
-            System.out.print("Now I'm repeating" +" "+ b +":");
+        System.out.println("This program gives quick tips on how to take better pictures and the amount of words");
 
-            System.out.println(z);
+        // This wordlist will randomize the tips, there's no steps
+        String [] wordlist = {"The more a scene is changing, the more pictures you should take", "If a football player is moving " +
+                ", hold down the shutter until he reaches touchdown", "Learn your camera, turn the flash on and off, select continuous shooting " +
+                "select the camera's time, select aperture priority, and shutter priority, View the historgram. "};
+        String [] cameraterms ={"Shutter", "Iso", "Aperture"};
 
-        }
+        String link = "<a href=\"https://www.behance.net/johnbel4585e51\" target=\"_blank\">http://abcd.efg.com</a>";
+        String link2 = "<a href=\"https://www.behance.net/johnbel4585e51\" target=\"_blank\">http://abcd.efg.com</a>";
+        String link3 = "<a href=\"https://www.exposureguide.com/top-10-digital-photography-tips/\" target=\"_blank\">http://abcd.efg.com</a>";
 
-        
+        String [] arraylink={link, link2, link3};
 
+        //finding out how many words
+        int oneLenght = wordlist.length;
+        int twolenght= cameraterms.length;
+        int threelenght= arraylink.length;
+
+
+        //generate random numbees
+        int rand1 =(int)(Math.random() * oneLenght);
+        int rand2= (int)(Math.random()*twolenght);
+        int rand3=(int)(Math.random() * threelenght);
+
+
+        String phrase= wordlist[rand1] + "  " + cameraterms[rand2] + "  " + arraylink[rand3];
+        System.out.println("Tips for photography:" +" " + phrase);
 
 
     }
